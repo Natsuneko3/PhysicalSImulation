29365f67061a4bbc5e46c906bc8cdcac84be1206
Natsuneko3 <kon3nian2ban@gmail.com>
2024-09-09 18:09:54 +0800

    update bloom
    (cherry picked from commit df23e0fcb320c8dfc1978849e3d4f5c5ce7096f2)
    
    # Conflicts:
    #	Shaders/DownSample.usf   resolved by [CherryPick] version
    #	Shaders/TextureBlur/Bloom.usf   resolved by [CherryPick] version
    #	Source/CustomPostProcess/Private/Bloom/TranslucentBloom.cpp   resolved by [CherryPick] version
    #	Source/CustomPostProcess/Private/Bloom/TranslucentBloom.h   resolved by [CherryPick] version
    #	Source/CustomPostProcess/Private/RenderAdapter.cpp   resolved by [CherryPick] version
    #	Source/CustomPostProcess/Public/RenderAdapter.h   resolved by [CherryPick] version

---
+++ Source/CustomPostProcess/Private/Bloom/TranslucentBloom.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "RenderAdapter.h"
+#include "TranslucentBloom.generated.h"
+class TranslucentBloom
+{
+public:
+
+};
+
+UCLASS(NotBlueprintable, MinimalAPI,DisplayName="Bloom")
+class UTranslucentBloom: public URenderAdapterBase
+{
+public:
+	GENERATED_BODY()
+	UTranslucentBloom();
+
+	UPROPERTY(Category = "RenderFeature",EditAnywhere,meta=(ClampMax=1))
+	float Size = 1.0f;
+
+	UPROPERTY(Category = "RenderFeature",EditAnywhere)
+	float Intensity = 0.1f;
+
+	UPROPERTY(Category = "RenderFeature",EditAnywhere)
+	float Falloff = 0.1f;
+
+	UPROPERTY(Category = "RenderFeature",EditAnywhere)
+	float BloomThreshold = 1.0f;
+	UPROPERTY(Category = "RenderFeature",EditAnywhere,meta=(ClampMin=1,ClampMax=10,UIMin=1,UIMax=10))
+	float BloomGlow = 10.0f;
+
+	UPROPERTY(Category = "RenderFeature",EditAnywhere)
+	float BloomLuminance = 1.0f;
+
+	UPROPERTY(Category = "RenderFeature",EditAnywhere,meta=(ClampMin=3,ClampMax=6,UIMin=3,UIMax=6))
+	uint32 BloomQuality = 4;
+
+	UPROPERTY(Category = "RenderFeature",EditAnywhere)
+	FLinearColor Color = FLinearColor::White;
+
+	virtual void PrePostProcessPass_RenderThread(FRDGBuilder& GraphBuilder, const FSceneView& View, const FPostProcessingInputs& Inputs) override;
+
+};
