1d51bc3c847e285efc6eb5ab89536321a700fe09
Natsuneko3 <kon3nian2ban@gmail.com>
2024-09-12 00:14:17 +0800

    fix
    (cherry picked from commit efa18d21ffff6ddc2e5659dfe890b14a44f9f290)

--- Source/CustomRenderFeature/Public/CustomRenderFeatureSceneProxy.h
+++ Source/CustomRenderFeature/Public/CustomRenderFeatureSceneProxy.h
@@ -22,7 +22,8 @@ public:
 protected:
 
 	virtual SIZE_T GetTypeHash() const override;
-	virtual void CreateRenderThreadResources() override;
+	virtual void CreateRenderThreadResources(FRHICommandListBase& RHICmdList) override;
+
 
 	virtual void DestroyRenderThreadResources() override;
 	virtual void GetDynamicMeshElements(const TArray<const FSceneView*>& Views, const FSceneViewFamily& ViewFamily, uint32 VisibilityMap, FMeshElementCollector& Collector) const override;
