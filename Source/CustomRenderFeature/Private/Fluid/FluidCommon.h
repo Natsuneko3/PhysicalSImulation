af41fcf2442e794728d9fe352059a7173c17d4fe
Natsuneko3 <kon3nian2ban@gmail.com>
2024-09-12 00:23:01 +0800

    fix for package

--- Source/CustomRenderFeature/Private/Fluid/FluidCommon.h
+++ Source/CustomRenderFeature/Private/Fluid/FluidCommon.h
@@ -1,5 +1,9 @@
 ﻿#pragma once
+#include "Engine/Texture.h"
+#include "ShaderParameterMacros.h"
+#include "SceneView.h"
 #include "FluidCommon.generated.h"
+
 #define COMPILEFLUID 0
 DECLARE_STATS_GROUP(TEXT("CustomRenderFeature"), STATGROUP_CustomRenderFeature, STATCAT_Advanced);
 
@@ -10,31 +14,6 @@ enum EShadertype
 	IteratePressure,
 	ComputeDivergence
 };
-
-BEGIN_SHADER_PARAMETER_STRUCT(FSolverBaseParameter, CUSTOMRENDERFEATURE_API)
-	SHADER_PARAMETER(FVector3f, GridSize)
-	SHADER_PARAMETER(int, Time)
-	SHADER_PARAMETER(float, dt)
-	SHADER_PARAMETER(float, dx)
-	SHADER_PARAMETER_SAMPLER(SamplerState, WarpSampler)
-	SHADER_PARAMETER_STRUCT_REF(FViewUniformShaderParameters, View)
-END_SHADER_PARAMETER_STRUCT()
-
-BEGIN_SHADER_PARAMETER_STRUCT(FFluidParameter, CUSTOMRENDERFEATURE_API)
-	SHADER_PARAMETER_STRUCT_INCLUDE(FSolverBaseParameter, SolverBaseParameter)
-	SHADER_PARAMETER(float, VorticityMult)
-	SHADER_PARAMETER(float, NoiseFrequency)
-	SHADER_PARAMETER(float, NoiseIntensity)
-	SHADER_PARAMETER(float, VelocityDissipate)
-	SHADER_PARAMETER(float, DensityDissipate)
-	SHADER_PARAMETER(float, GravityScale)
-SHADER_PARAMETER(int, UseFFT)
-SHADER_PARAMETER(FVector3f, WorldPosition)
-SHADER_PARAMETER(FVector3f, WorldVelocity)
-	SHADER_PARAMETER_TEXTURE(Texture2D, InTexture)
-SHADER_PARAMETER_SAMPLER(SamplerState, SimSampler)
-SHADER_PARAMETER_SAMPLER(SamplerState, InTextureSampler)
-END_SHADER_PARAMETER_STRUCT()
 
 USTRUCT(BlueprintType)
 struct FPlandFluidParameters
@@ -83,3 +62,28 @@ struct FLiquidSolverParameter
 	UPROPERTY(EditAnywhere, Category = "LiquidSolverParameter")
 	float GravityScale = 20;
 };
+
+BEGIN_SHADER_PARAMETER_STRUCT(FSolverBaseParameter,)
+	SHADER_PARAMETER(FVector3f, GridSize)
+	SHADER_PARAMETER(int, Time)
+	SHADER_PARAMETER(float, dt)
+	SHADER_PARAMETER(float, dx)
+	SHADER_PARAMETER_SAMPLER(SamplerState, WarpSampler)
+	SHADER_PARAMETER_STRUCT_REF(FViewUniformShaderParameters, View)
+END_SHADER_PARAMETER_STRUCT()
+
+BEGIN_SHADER_PARAMETER_STRUCT(FFluidParameter, )
+	SHADER_PARAMETER_STRUCT_INCLUDE(FSolverBaseParameter, SolverBaseParameter)
+	SHADER_PARAMETER(float, VorticityMult)
+	SHADER_PARAMETER(float, NoiseFrequency)
+	SHADER_PARAMETER(float, NoiseIntensity)
+	SHADER_PARAMETER(float, VelocityDissipate)
+	SHADER_PARAMETER(float, DensityDissipate)
+	SHADER_PARAMETER(float, GravityScale)
+	SHADER_PARAMETER(int, UseFFT)
+	SHADER_PARAMETER(FVector3f, WorldPosition)
+	SHADER_PARAMETER(FVector3f, WorldVelocity)
+	SHADER_PARAMETER_TEXTURE(Texture2D, InTexture)
+	SHADER_PARAMETER_SAMPLER(SamplerState, SimSampler)
+	SHADER_PARAMETER_SAMPLER(SamplerState, InTextureSampler)
+END_SHADER_PARAMETER_STRUCT()
