438f98bf8cca26c3071306dcb93c50a99b7a951a
Natsuneko3 <kon3nian2ban@gmail.com>
2024-09-13 00:23:32 +0800

    Update to 5.3

--- Package.bat
+++ Package.bat
@@ -1,8 +1,8 @@
 SET PluginPath="D:\OneDrive\UnrealEnginePlugins\PhysicalSImulation\CustomRenderFeature.uplugin"
 SET PackagePath="D:\OneDrive\UnrealEnginePlugins\Package\CustomRenderFeature5_2"
 D:
-cd "D:\Epic Games\UE_5.2\Engine\Build\BatchFiles"
+cd "D:\Epic Games\UE_5.3\Engine\Build\BatchFiles"
 call RunUAT.bat BuildPlugin -Plugin=%PluginPath% -Package=%PackagePath% -Rocket
 cd %PackagePath%
-7z.exe a -r CustomRenderFeature5_2.zip
-MOVE CustomRenderFeature5_2.zip ..\
+7z.exe a -r CustomRenderFeature5_3.zip
+MOVE CustomRenderFeature5_3.zip ..\
